8f25ebf7776ee1117f6cf3515e3181dcad7658ec
Connor McCue <connor.mccue@comcast.net>
2022-07-21 16:18:35 -0700

    chilis updated

--- app.js
+++ app.js
@@ -8,16 +8,35 @@ const askButton = document.getElementById('ask-button');
 const answerSection = document.getElementById('answer-section');
 const questionDisplay = document.getElementById('question-display');
 const answerDisplay = document.getElementById('answer-display');
-console.log(questionSection.classList);
 const crystalBall = document.getElementById('crystal-ball');
 
+
+const picZero = document.getElementById('pic0');
+const picOne = document.getElementById('pic1');
+const picTwo = document.getElementById('pic2');
+const picThree = document.getElementById('pic3');
+const picFour = document.getElementById('pic4');
+const picFive = document.getElementById('pic5');
+const picSix = document.getElementById('pic6');
+const picSeven = document.getElementById('pic7');
+const picEight = document.getElementById('pic8');
+const picNine = document.getElementById('pic9');
+const picTen = document.getElementById('pic10');
+const picEleven = document.getElementById('pic11');
+
 const answers = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
+    'Chips and Salsa',        
+    'Chicken Fajitas', 
+    'OldTimer with Cheese',
+    'Steak Fajitas',
+    'Chicken Crispers',
+    'Baby Back Ribs ',
+    'Queso Burger',
+    'Ultimate Smokehouse Combo',
+    'Southwestern Eggrolls',
+    'Cajun Shrimp Pasta',
+    'Chipotle Shrimp Fresh Mex Bowl',
+    'Big Mouth Crispy Chicken Sandwich',
 ];
 
 // set event listeners 
@@ -31,22 +50,57 @@ askButton.addEventListener('click', () => {
 
 function copyQuestion() {
     const question = askedQuestion.value;
-    questionDisplay.textContent = question;
+    questionDisplay.textContent = "I hear you like " + question + ".";
 }
 
 function toggleVisibility() {
     questionSection.classList.add('hidden');
     answerSection.classList.remove('hidden');
 }
-
 function getRandomItem(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     const item = array[randomIndex];
+    if (randomIndex === 0) {
+        picZero.classList.remove('hidden');
+    }  
+    if (randomIndex === 1) {
+        picOne.classList.remove('hidden');
+    }  
+    if (randomIndex === 2) {
+        picTwo.classList.remove('hidden');
+    }  
+    if (randomIndex === 3) {
+        picThree.classList.remove('hidden');
+    }  
+    if (randomIndex === 4) {
+        picFour.classList.remove('hidden');
+    }  
+    if (randomIndex === 5) {
+        picFive.classList.remove('hidden');
+    }  
+    if (randomIndex === 6) {
+        picSix.classList.remove('hidden');
+    }  
+    if (randomIndex === 7) {
+        picSeven.classList.remove('hidden');
+    }  
+    if (randomIndex === 8) {
+        picEight.classList.remove('hidden');
+    }  
+    if (randomIndex === 9) {
+        picNine.classList.remove('hidden');
+    }  
+    if (randomIndex === 10) {
+        picTen.classList.remove('hidden');
+    }  
+    if (randomIndex === 11) {
+        picEleven.classList.remove('hidden');
+    }  
     return item;
 }
 function showRandomAnswer(){
     const randomAnswer = getRandomItem(answers);
-    answerDisplay.textContent = randomAnswer;
+    answerDisplay.textContent = "Why not try the " + randomAnswer + "?";
 }
 
 function applyAnimations(){
